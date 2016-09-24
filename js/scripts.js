11a908a7d8a60f1d1a36fe88bd319469ace297d8
Matthew Flebbe <matt.flebbe@pmharbor.com>
2016-09-24 14:12:00 -0400

    beginning footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,31 @@
-// Empty JS for your own code to be here
+;( function( $ ) {
+	"use strict";
+
+	$( ".bar" ).each( function() {
+
+		var $bar = $( this ),
+			 $pct = $bar.find( ".pct" ),
+			 data = $bar.data( "bar" );
+
+		setTimeout( function() {
+
+			$bar
+				.css( "background-color", data.color )
+				.animate({
+					"width": $pct.html()
+				}, data.speed || 3000, function() {
+
+					$pct.css({
+						"color": data.color,
+						"opacity": 1
+					});
+
+				});
+
+		}, data.delay || 0 );
+
+	});
+
+})( jQuery );
+
+(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}id=id+'_';if(!gi.call(d,id)){qs=ce.call(d,'link');qs.rel='stylesheet';qs.id=id;qs.href=b+'share-button.css';s=gt.call(d,'head')[0];s.appendChild(qs,s)}})()
